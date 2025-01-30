035a2bccdfaf45f0c0d351650844e7d96ef93ce2
ArnaldoBetancourt <arnaldobetancourt@hotmail.com>
2025-01-30 07:47:09 -0500

    creando el arreglo y la funcion para ingresar amigos

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// arreglo para lamacenar los nombres de los amigos
+let amigosIngresados = [];
+//función para ingresar amigos
+function agregarAmigo(){
+    let nombre= nombreDeAmigo.value; 
+    if(nombre=== " "){
+        alert("por favor,inserte un nombre");
+        return;
+    }
+    amigosIngresados.push(nombre);
+}
